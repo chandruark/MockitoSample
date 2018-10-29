6cf15f5659b6123c260f366a8705631e9f27e721
Chandru <chandru@nfnlabs.com>
2018-10-29 11:52:42 +0530

    Initial Setup and Login Module Sample test completed

---
+++ app/src/main/java/in/nfnlabs/mymockito/LandingPageActivity.java
@@ -0,0 +1,15 @@
+package in.nfnlabs.mymockito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+
+    }
+}
